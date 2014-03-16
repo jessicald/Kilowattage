b94866488d2a6a7a79ea56c6bd2bb5c92780a28e
Joseph Leeper <jl933a@lab.icc.edu>
2014-03-16 15:58:04 -0500

    Don't parse arguments if this isn't called from a command line.

--- kilowattage.py
+++ kilowattage.py
@@ -7,40 +7,6 @@ __desc__ = ('Calculates unknowns in utility pricing and usage.')
 __url__ = 'https://github.com/cordella/Kilowattage'
 
 
-import argparse
-
-def parse_args():
-    """ Set up, and return the results of, the argument parser. """
-    parser = argparse.ArgumentParser(
-        add_help=False, prog=__title__,
-        usage='%(prog)s [options] [logging] network|ALL')
-
-    optional = parser.add_argument_group(title='options')
-    log = parser.add_argument_group(title='logging')
-
-    optional.add_argument('-h', '--help', action='help',
-                          help='print this help and exit')
-    optional.add_argument('-v', '--version', action='version',
-                          version='%(prog)s '+__version__,
-                          help="show %(prog)s's version and exit")
-#    optional.add_argument('-c', '--config', dest='config', default=None,
-#                          help='specify an alternative configuration')
-#    optional.add_argument('-d', '--daemon', dest='daemonise',
-#                          action='store_true',
-#                          help='start %(prog)s in daemon mode')
-#    optional.add_argument('-p', '--pid', dest='pidfile', metavar='FILE',
-#                          default=None, help='keep a pid file')
-
-#    log.add_argument('-o', '--log', metavar='[FILE]', dest='logfile',
-#                     default=None, help="log in FILE")
-#    log.add_argument('-a', '--append', dest='logappend', action='store_true',
-#                     help="append to FILE when logging")
-
-#    parser.add_argument('network',
-#                        help='an IRC network profile in your configuration')
-
-    return parser.parse_args()
-
 def separate_time(undivided_time, is_seconds = False):
     """ Given the time in hours or seconds, returns the time in common divisions,
         i.e. days, hours, minutes, seconds
@@ -65,7 +31,43 @@ def calculate_time(cents_per_kWh, wattage, dollar_amount):
 
 
 if __name__ == '__main__':
-    args = parse_args()
+
+    import argparse
+
+    def parse_args():
+        """ Set up, and return the results of, the argument parser. """
+        parser = argparse.ArgumentParser(
+            add_help=False, prog=__title__,
+            usage='%(prog)s [options] [logging] network|ALL')
+
+        optional = parser.add_argument_group(title='options')
+        log = parser.add_argument_group(title='logging')
+
+        optional.add_argument('-h', '--help', action='help',
+                              help='print this help and exit')
+        optional.add_argument('-v', '--version', action='version',
+                              version='%(prog)s '+__version__,
+                              help="show %(prog)s's version and exit")
+    #    optional.add_argument('-c', '--config', dest='config', default=None,
+    #                          help='specify an alternative configuration')
+    #    optional.add_argument('-d', '--daemon', dest='daemonise',
+    #                          action='store_true',
+    #                          help='start %(prog)s in daemon mode')
+    #    optional.add_argument('-p', '--pid', dest='pidfile', metavar='FILE',
+    #                          default=None, help='keep a pid file')
+
+    #    log.add_argument('-o', '--log', metavar='[FILE]', dest='logfile',
+    #                     default=None, help="log in FILE")
+    #    log.add_argument('-a', '--append', dest='logappend', action='store_true',
+    #                     help="append to FILE when logging")
+
+    #    parser.add_argument('network',
+    #                        help='an IRC network profile in your configuration')
+
+        return parser.parse_args()
+
+
+#    args = parse_args()
 
 #    print(separate_time(3.034166666666666667))
     # [Out] (0, 3, 2, 3.0)
@@ -74,4 +76,4 @@ if __name__ == '__main__':
 
 #    print("{:0.5g} {:s}".format((calculate_time(4.271, 60, 1)), 'hours'))
 #    print("{0:d} days, {1:d} hours, {2:d} minutes, {3:f} seconds".format(separate_time(calculate_time(4.271, 60, 1))))
-    print("{:d} days, {:d} hours, {:d} minutes, {:0.3f} seconds".format(*separate_time(calculate_time(4.271, 60, 1))))
+    print("{:d} days, {:d} hours, {:d} minutes, {:0.3f} seconds".format(*separate_time(calculate_time(4.271, 180, 1))))
